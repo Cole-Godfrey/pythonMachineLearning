fc69f2898078080b5dd094db5bf47679a135b99d
Cole Godfrey <coleg@Coles-MacBook-Pro.local>
2026-03-19 13:19:37 -0700

    Finished linear regression.

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 This is code I am writing in a conda environment to learn machine learning in Python using TensorFlow.
 
-Here is what I've done so far:
-- Created a conda environment and set up the necessary dependencies (tensorEnv)
+Here is what I've learned so far:
 - Linear Regression
 
--- linearRegression/regression.py
+++ linearRegression/regression.py
@@ -7,8 +7,21 @@ from sklearn.utils import shuffle
 data = pd.read_csv("student-mat.csv", sep=";")
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 predict = "G3"
-X = np.array(data.drop([predict], 1))
+X = np.array(data.drop(columns=[predict]))
 y = np.array(data[predict])
-X_train, y_train, X_test, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+XTrain, XTest, yTrain, yTest = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+linear = linear_model.LinearRegression()
+linear.fit(XTrain, yTrain)
+acc = linear.score(XTest, yTest)
+
+print(acc)
+print("Co: ", linear.coef_)
+print("Intercept: ", linear.intercept_)
+
+predictions = linear.predict(XTest)
+
+for x in range(len(predictions)):
+    print(predictions[x], XTest[x], yTest[x])
+
 
 
