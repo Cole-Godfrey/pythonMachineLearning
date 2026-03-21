fadd256ea66f8e89b487d7c301e22e273a98ae16
Cole Godfrey <coleg@Coles-MacBook-Pro.local>
2026-03-20 20:12:49 -0700

    Set up train test split for KNN

--- linearRegression/regression.py
+++ linearRegression/regression.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 import pickle
 from matplotlib import style
